c8e4211a87b6efdabff4820d3ac2c9ac5225f317
Andrew Beeken <abeeken@gmail.com>
2019-03-20 16:36:22 +0000

    Added about page

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ site.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
+site.get('/about', function(req, res){
+    res.sendFile(__dirname + '/public/about.html');
+});
+
 site.listen(port, () => console.log(`App listening on port ${port}`))
