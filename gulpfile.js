c8ba3984349f7fb233ae7c96aa99a84fdbf7f909
Andrew Beeken <abeeken@gmail.com>
2019-03-25 11:20:38 +0000

    Removed public files and added to GitIgnore

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 
+
+
 gulp.task('views', function() {
     return gulp.src('src/views/*.pug')
         .pipe(pug({}))
---
+++ src/js/example.js
@@ -0,0 +1,1 @@
+// Example JS file - make sure you edit the gulpfile as you change/add JS
